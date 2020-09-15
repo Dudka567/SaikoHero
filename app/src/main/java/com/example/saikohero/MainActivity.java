9c2b6404a159249deda41407d36b005b3bbf9607
Byoliee <mu.korepanov@gmail.com>
2020-09-15 18:57:49 +0300

    хз что

--- app/src/main/java/com/example/saikohero/MainActivity.java
+++ app/src/main/java/com/example/saikohero/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.saikohero;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
@@ -26,5 +28,14 @@ public class MainActivity extends AppCompatActivity {
         FonDJ=MediaPlayer.create(this,R.raw.menu);
         FonDJ.start();
 
+        btnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlphaAnimation alphaAnimation = new AlphaAnimation(1.0f, 0f);
+                alphaAnimation.setDuration(600);
+                btnMenu.startAnimation(alphaAnimation);
+                btnMenu.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
